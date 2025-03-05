68d48439c1c6beaf216fa5ac8a8a485ad6019784
mina126 <minamobn1@gmail.com>
2025-03-05 18:25:00 +0200

    Add files via upload

---
+++ assets/CodeSQL/duplicates.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM (
+	SELECT company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
+			) AS row_num
+	FROM 
+		world_layoffs.layoffs_staging
+) duplicates
+WHERE 
+	row_num > 1;
