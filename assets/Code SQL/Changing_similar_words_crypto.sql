f204c5d6c0be004b2bc76457d524c860dc2094b1
mina126 <minamobn1@gmail.com>
2025-03-05 17:28:38 +0200

    Add files via upload

---
+++ assets/Code SQL/Changing_similar_words_crypto.sql
@@ -0,0 +1,13 @@
+-- I also noticed the Crypto has multiple different variations. We need to standardize that - let's say all to Crypto
+SELECT DISTINCT industry
+FROM world_layoffs.layoffs_staging2
+ORDER BY industry;
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry IN ('Crypto Currency', 'CryptoCurrency');
+
+-- now that's taken care of:
+SELECT DISTINCT industry
+FROM world_layoffs.layoffs_staging2
+ORDER BY industry;
